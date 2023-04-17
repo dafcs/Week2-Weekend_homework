816ed8431aa5e4325454dcd85fb4a80d50806728
Daniel FCS <danielf.cs@outlook.com>
2023-04-17 10:28:10 +0100

    moved MVP away

--- MVP/run_tests.py
+++
@@ -1,6 +0,0 @@
-import unittest
-from tests.guest_test import TestGuest
-from tests.room_test import TestRoom
-
-if __name__ == '__main__':
-    unittest.main()
--- MVP/src/guest.py
+++
@@ -1,7 +0,0 @@
-class Guest:
-    def __init__(self,name,cash,fav_song,fav_genre):
-        self.name = name
-        self.cash = cash
-        
-    def reduce_cash(self,amount):
-        self.cash -= amount
